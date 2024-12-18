51c79e847e5ce3402861f50ab00ea673ac4e6ea3
tango4ik <144614467+tango4ik@users.noreply.github.com>
2024-12-18 21:20:10 +0530

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,35 @@
+class acm():
+    def __init__(self,code:list[str]):
+        self.code :list[str] = code
+        self.stack: int = None
+    def do(self):
+        i = 0
+        while i < len(self.code):
+            bit = self.code[i]
+            if "&" in bit:
+                if self.stack == int(bit.removeprefix("&")):
+                    i = i
+                else:
+                    i += 1
+            if "out" in bit:
+                print(chr(self.stack),end="")
+            elif "in" in bit:
+                self.stack = ord(input())
+            elif bit.isdigit():
+                self.stack = int(bit)
+            elif "goto" in bit:
+                i = int(bit.removeprefix("goto ")) - 1
+            elif "end" in bit:
+                break
+            i += 1
+code = []
+program = input()
+with open(program + '.acm') as f:
+    code = f.readlines()
+inp = None
+#while inp != "end":
+#   inp = input()
+#   if inp != "end":
+#     code.append(inp)
+comp = acm(code)
+comp.do()
